59a98e08767d24366f24d976107144984213ca15
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-03-02 18:12:45 -0600

    Login Page as index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
 
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title> Fast Fresh Foods Market | Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    </head>
+
+    <body style="background-color: #031D44">
+        <form action="#" method="post">
+            <a class="navbar-brand text-dark" href="index.php">
+                <h1>Fast Fresh Foods Market</h1>
+            </a>
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="password">
+            <input type="submit" name="submit" value="Login">
+            <p>
+        </form>
+    </body>
+
+</html>
